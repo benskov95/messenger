ab5f8c27cf531c0bef8bf96651fb82401d0171eb
Benjamin S <ben.sommer.skovgaard@gmail.com>
2022-09-14 20:04:26 +0200

    renamed main file in socket-server

--- messenger-frontend/socket-server/socketServer.js
+++ messenger-frontend/socket-server/socketServer.js
@@ -1,7 +1,7 @@
 const express = require("express")
 const socketIo = require("socket.io")
 const https = require("http")
-const PORT = process.env.PORT || 9000;
+const PORT = process.env.PORT || 5001;
 
 const app = express();
 const server = https.createServer(app);
